7b40a59b2c1605caf73cc5d337a9d5561cdd4e8d
zkeller383 <99515509+zkeller383@users.noreply.github.com>
2024-10-13 20:28:31 -0400

    Update inventory_service.py

--- services/inventory_service.py
+++ services/inventory_service.py
@@ -1,9 +1,32 @@
 # services/inventory_service.py
 # Inventory Management Service
 
-class InventoryManagementService:
-    def update_inventory(self, item_id, quantity, location):
-        pass  # Placeholder for updating inventory
+import sqlite3
 
-    def get_inventory(self, item_id):
-        pass  # Placeholder for retrieving inventory information
+class InventoryManagementService:
+    
+    def update_inventory (self, equipment_id, name, quantity, warehouse_location):
+        connection = sqlite3.connect('equipment_tracking.db')
+        cursor = connection.cursor()
+        
+        result = cursor.exectute("SELECT equipment_id FROM equipment where equipment_id = ?", (equipment_id))
+        if result.fetchone() == equipment_id:
+            cursor.execute("UPDATE equipment SET quantity = ? , SET warehouse_location = ? , SET name = ? WHERE equipment_id = ?", (quantity, warehouse_location, name, equipment_id))
+        else:
+            return("Item not found")
+        connection.commit()
+        connection.close()
+        
+    def get_inventory(self, equipment_id):
+        connection = sqlite3.connect('equipment_tracking.db')
+        cursor = connection.cursor()
+        
+        item = InventoryItem()
+        
+        result = cursor.exectue("SELECT eqipment_id, name, quantity, warehouse_location FROM equipment WHERE equipment_id = ?"(equipment_id))
+        result.fetchone()
+        
+        item.id = result[0]
+        item.name = result[1]
+        item.quantity = result[2]
+        item.warehouse_location = result[3]
